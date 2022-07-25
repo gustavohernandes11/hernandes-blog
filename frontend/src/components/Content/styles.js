58e710262a2be1bc3fc484db7ebdc0ca15e0334c
gustavohernandes11 <gustavo.hernandes.s11@gmail.com>
2022-07-25 11:03:05 -0300

    styles: .

--- frontend/src/components/Content/styles.js
+++ frontend/src/components/Content/styles.js
@@ -4,7 +4,7 @@ export const Container = styled.main`
     ${({ theme }) => css`
         background-color: ${theme.colors.background};
         color: ${theme.colors.text};
-        padding: ${theme.spacings[5]} 10%;
+        padding: ${theme.spacings[5]} 15%;
         overflow-y: auto;
         display: flex;
         flex-direction: column;
