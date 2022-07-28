0416d462d716ea37053afee283a140aeccda7015
gustavohernandes11 <gustavo.hernandes.s11@gmail.com>
2022-07-28 10:35:14 -0300

    CI: adicionado sincronização como cloudinary para arquivos de mídia

--- backend/config/plugins.js
+++ backend/config/plugins.js
@@ -2,4 +2,18 @@ module.exports = ({ env }) => ({
 	publisher: {
 		enabled: true,
 	},
+	upload: {
+        config: {
+          provider: 'cloudinary',
+          providerOptions: {
+            cloud_name: env('CLOUDINARY_NAME'),
+            api_key: env('CLOUDINARY_KEY'),
+            api_secret: env('CLOUDINARY_SECRET'),
+          },
+          actionOptions: {
+            upload: {},
+            delete: {},
+          },
+        },
+      },
 });
