d3f8c59e93ee72f2b8964e5ba0ac119c9eddb223
gustavohernandes11 <gustavo.hernandes.s11@gmail.com>
2022-07-28 21:38:21 -0300

    tradução de nome da pasta

---
+++ frontend/pages/sobre/index.tsx
@@ -0,0 +1,49 @@
+import { Content } from "../../src/components/Content";
+import { Heading } from "../../src/components/Heading";
+
+import type { NextPage } from "next";
+import Head from "next/head";
+import { Typography } from "@mui/material";
+
+const About: NextPage = () => {
+    return (
+        <>
+            <Head>
+                <title>.blog</title>
+                <meta
+                    name="description"
+                    content="Blog to share develevopment tips and ticks of Javascript, Typscript, Html, Node, CSS"
+                />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <Content >
+                <Heading>
+                    About
+                </Heading>
+                <Typography mt={3} paragraph >
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                    Voluptas quod harum accusantium quam illum dignissimos
+                    fugiat, doloribus ullam, laborum id ipsa est. Debitis,
+                    quisquam. Distinctio laborum dolore exercitationem aperiam
+                    reiciendis.
+                </Typography>
+                <Typography paragraph >
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                    Voluptas quod harum accusantium quam illum dignissimos
+                    fugiat, doloribus ullam, laborum id ipsa est. Debitis,
+                    quisquam. Distinctio laborum dolore exercitationem aperiam
+                    reiciendis.
+                </Typography>
+                <Typography paragraph >
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                    Voluptas quod harum accusantium quam illum dignissimos
+                    fugiat, doloribus ullam, laborum id ipsa est. Debitis,
+                    quisquam. Distinctio laborum dolore exercitationem aperiam
+                    reiciendis.
+                </Typography>
+            </Content>
+        </>
+    );
+};
+
+export default About;
