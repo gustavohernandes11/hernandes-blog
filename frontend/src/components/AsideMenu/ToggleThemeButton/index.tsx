52f6719c2e61dc4a0ef62cc4a3fd0e7334208cbb
gustavohernandes11 <gustavo.hernandes.s11@gmail.com>
2022-07-31 14:37:21 -0300

    test: testes unitários

--- frontend/src/components/AsideMenu/ToggleThemeButton/index.tsx
+++ frontend/src/components/AsideMenu/ToggleThemeButton/index.tsx
@@ -5,11 +5,7 @@ import { IconButton } from "@mui/material";
 export const ToggleThemeButton = ({ onClick }: any) => {
     return (
         <IconButton onClick={onClick} color="inherit">
-            <LightBulb
-                width={25}
-                height={25}
-                aria-label="toggle-theme-button"
-            />
+            <LightBulb aria-label="toggle-theme-button" width={25} height={25} />
         </IconButton>
     );
 };
