7e84a13716f0f38fad52d5ca9f6198b10aa416bd
gustavohernandes11 <gustavo.hernandes.s11@gmail.com>
2022-08-02 18:45:03 -0300

    style: variant de Title modificada para h4

--- frontend/src/components/Title/index.tsx
+++ frontend/src/components/Title/index.tsx
@@ -2,21 +2,19 @@ import { Typography } from "@mui/material";
 
 export const Title = ({
     children,
-    variant = "h3",
+    variant = "h4",
     component = "h1",
     className = "title",
     fontWeight = "800",
     mb = 5,
-}: any): JSX.Element => {
-    return (
-        <Typography
-            variant={variant}
-            component={component}
-            className={className}
-            fontWeight={fontWeight}
-            mb={mb}
-        >
-            {children}
-        </Typography>
-    );
-};
+}: any): JSX.Element => (
+    <Typography
+        component={component}
+        className={className}
+        fontWeight={fontWeight}
+        mb={mb}
+        variant={variant}
+    >
+        {children}
+    </Typography>
+);
