1024cdcb4dddc467802e5d65a7a99ac9ca9904e4
gustavohernandes11 <gustavo.hernandes.s11@gmail.com>
2022-08-02 12:03:44 -0300

    fix> bold não estava sendo aplicado

--- frontend/src/components/Title/index.tsx
+++ frontend/src/components/Title/index.tsx
@@ -5,18 +5,18 @@ export const Title = ({
     variant = "h3",
     component = "h1",
     className = "title",
-    fontWeight = "bold",
+    fontWeight = "800",
     mb = 5,
 }: any): JSX.Element => {
-        return (
-            <Typography
-                variant={variant}
-                component={component}
-                className={className}
-                fontWeight={fontWeight}
-                mb={mb}
-            >
-                {children}
-            </Typography>
-        );
-    };
+    return (
+        <Typography
+            variant={variant}
+            component={component}
+            className={className}
+            fontWeight={fontWeight}
+            mb={mb}
+        >
+            {children}
+        </Typography>
+    );
+};
