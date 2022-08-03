f2b3b7e3ada7722e7950980f2a2e919b72445336
gustavohernandes11 <gustavo.hernandes.s11@gmail.com>
2022-08-03 18:45:53 -0300

    fix: ajuste no componente de Seo: seus dados agora estão mais organizados por página e não apenas no arquivo _document. Melhoria em como as tags são aplicadas pelo componente seo

--- frontend/src/components/Seo/index.tsx
+++ frontend/src/components/Seo/index.tsx
@@ -1,18 +1,33 @@
+import { useEffect, useState } from "react";
 import Head from "next/head";
 
-export const Seo = () => {
-    let meta_description, meta_title, meta_keywords;
+interface MetaType {
+    meta_title: string | null | undefined;
+    meta_description: string | null | undefined;
+    meta_keywords: string | null | undefined;
+}
 
+export const Seo = ({ metadata = {} }: any) => {
+    const [metaState] = useState(metadata);
     return (
         <Head>
-            <meta name="title" content={meta_title} />
+            {metaState.meta_title && (
+                <>
+                    <meta name="title" content={metaState.meta_title} />
+                    <title>{metaState.meta_title}</title>
+                </>
+            )}
+
             <meta name="rating" content="general" />
-            <meta name="og:description" content={meta_description} />
-            <meta name="keywords" content={meta_keywords} />
-            <meta name="robots" content="index, follow" />
-            <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
-            <meta name="revisit-after" content="3 days" />
-            <meta name="author" content="Gustavo Hernandes" />
+            {metaState.meta_description && (
+                <meta
+                    name="og:description"
+                    content={metaState.meta_description}
+                />
+            )}
+            {metaState.meta_keywords && (
+                <meta name="keywords" content={metaState.meta_keywords} />
+            )}
         </Head>
     );
 };
