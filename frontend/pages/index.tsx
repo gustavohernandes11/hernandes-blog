a720488f0c8da315be70255f0c234efdf30b3d6b
gustavohernandes11 <gustavo.hernandes.s11@gmail.com>
2022-11-14 17:50:03 -0300

    ajuste nas páginas de acordo com o design

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,88 +1,90 @@
 import type { NextPage } from "next";
-import { Logo } from '../src/components/Logo'
-import { Header } from '../src/components/Header'
+import { HomeHeader } from '../src/components/HomeHeader'
 import { ArticleList } from '../src/components/ArticleList'
 import { Article } from "components/Article";
 import { Pagination } from "components/Pagination";
 const Home: NextPage = (): any => {
     return (
         <>
-            <Header />
-            <ArticleList>
-                <Article
-                    categoryInitial="JS"
-                    title="Orientação a Objetos com Javascript"
-                    color="yellow"
-                    date="12 de março de 2022"
-                    excerpt="lorem ipsum bla bla bla"
-                    readingTime="3min"
-                    key={1} />
-                <Article
-                    categoryInitial="EX"
-                    title="Vantagens e desvantagens da linguagem Elixir."
-                    color="#6181F1"
-                    date="12 de fevereiro de 2022"
-                    excerpt="lorem ipsum bla bla bla"
-                    readingTime="12min"
-                    key={2} />
-                <Article
-                    categoryInitial="TS"
-                    title="Typescript: principais conceitos"
-                    color="blue"
-                    date="5 de julho de 2022"
-                    excerpt="lorem ipsum bla bla bla"
-                    readingTime="10min"
-                    key={3} />
-                <Article
-                    categoryInitial="JS"
-                    title="Orientação a Objetos com Javascript"
-                    color="yellow"
-                    date="12 de março de 2022"
-                    excerpt="lorem ipsum bla bla bla"
-                    readingTime="3min"
-                    key={1} />
-                <Article
-                    categoryInitial="EX"
-                    title="Vantagens e desvantagens da linguagem Elixir."
-                    color="#6181F1"
-                    date="12 de fevereiro de 2022"
-                    excerpt="lorem ipsum bla bla bla"
-                    readingTime="12min"
-                    key={2} />
-                <Article
-                    categoryInitial="TS"
-                    title="Typescript: principais conceitos"
-                    color="blue"
-                    date="5 de julho de 2022"
-                    excerpt="lorem ipsum bla bla bla"
-                    readingTime="10min"
-                    key={3} />
-                <Article
-                    categoryInitial="JS"
-                    title="Orientação a Objetos com Javascript"
-                    color="yellow"
-                    date="12 de março de 2022"
-                    excerpt="lorem ipsum bla bla bla"
-                    readingTime="3min"
-                    key={1} />
-                <Article
-                    categoryInitial="EX"
-                    title="Vantagens e desvantagens da linguagem Elixir."
-                    color="#6181F1"
-                    date="12 de fevereiro de 2022"
-                    excerpt="lorem ipsum bla bla bla"
-                    readingTime="12min"
-                    key={2} />
-                <Article
-                    categoryInitial="TS"
-                    title="Typescript: principais conceitos"
-                    color="blue"
-                    date="5 de julho de 2022"
-                    excerpt="lorem ipsum bla bla bla"
-                    readingTime="10min"
-                    key={3} />
-            </ArticleList>
-            <Pagination count={10} page={1} onChange={() => { }} />
+            <HomeHeader />
+            <section className="margin">
+                <ArticleList>
+                    <Article
+                        categoryInitial="JS"
+                        title="Orientação a Objetos com Javascript"
+                        color="#F6E653"
+                        date="12 de março de 2022"
+                        excerpt="lorem ipsum bla bla bla"
+                        readingTime="3min"
+                        key={1} />
+                    <Article
+                        categoryInitial="EX"
+                        title="Vantagens e desvantagens da linguagem Elixir."
+                        color="#BA49FF"
+                        date="12 de fevereiro de 2022"
+                        excerpt="lorem ipsum bla bla bla"
+                        readingTime="12min"
+                        key={2} />
+                    <Article
+                        categoryInitial="TS"
+                        title="Typescript: principais conceitos"
+                        color="#6181F1"
+                        date="5 de julho de 2022"
+                        excerpt="lorem ipsum bla bla bla"
+                        readingTime="10min"
+                        key={3} />
+                    <Article
+                        categoryInitial="JS"
+                        title="Orientação a Objetos com Javascript"
+                        color="#F6E653"
+                        date="12 de março de 2022"
+                        excerpt="lorem ipsum bla bla bla"
+                        readingTime="3min"
+                        key={1} />
+                    <Article
+                        categoryInitial="EX"
+                        title="Vantagens e desvantagens da linguagem Elixir."
+                        color="#BA49FF"
+                        date="12 de fevereiro de 2022"
+                        excerpt="lorem ipsum bla bla bla"
+                        readingTime="12min"
+                        key={2} />
+                    <Article
+                        categoryInitial="TS"
+                        title="Typescript: principais conceitos"
+                        color="#6181F1"
+                        date="5 de julho de 2022"
+                        excerpt="lorem ipsum bla bla bla"
+                        readingTime="10min"
+                        key={3} />
+                    <Article
+                        categoryInitial="JS"
+                        title="Orientação a Objetos com Javascript"
+                        color="#F6E653"
+                        date="12 de março de 2022"
+                        excerpt="lorem ipsum bla bla bla"
+                        readingTime="3min"
+                        key={1} />
+                    <Article
+                        categoryInitial="EX"
+                        title="Vantagens e desvantagens da linguagem Elixir."
+                        color="#BA49FF"
+                        date="12 de fevereiro de 2022"
+                        excerpt="lorem ipsum bla bla bla"
+                        readingTime="12min"
+                        key={2} />
+                    <Article
+                        categoryInitial="TS"
+                        title="Typescript: principais conceitos"
+                        color="#6181F1"
+                        date="5 de julho de 2022"
+                        excerpt="lorem ipsum bla bla bla"
+                        readingTime="10min"
+                        key={3} />
+                </ArticleList>
+                <Pagination count={10} page={1} onChange={() => { }} />
+            </section>
+
         </>
     );
 };
--- frontend/pages/posts/[slug].tsx
+++ frontend/pages/posts/[slug].tsx
@@ -1,6 +1,59 @@
+import { Article } from "components/Article";
+import { Header } from "components/Header";
+import { Heading } from "components/Heading";
+import { SliderComponent } from "components/Slider";
+
+const items = [
+    <Article
+        categoryInitial="TS"
+        title="Typescript: principais conceitos"
+        color="#6181F1"
+        date="5 de julho de 2022"
+        excerpt="lorem ipsum bla bla bla"
+        readingTime="10min"
+        key={3}
+        vertical={true}
+    />,
+    <Article
+        categoryInitial="JS"
+        title="Orientação a Objetos com Javascript"
+        color="#F6E653"
+        date="12 de março de 2022"
+        excerpt="lorem ipsum bla bla bla"
+        readingTime="3min"
+        key={1}
+        vertical={true} />,
+    <Article
+        categoryInitial="EX"
+        title="Vantagens e desvantagens da linguagem Elixir."
+        color="#BA49FF"
+        date="12 de fevereiro de 2022"
+        excerpt="lorem ipsum bla bla bla"
+        readingTime="12min"
+        key={2}
+        vertical={true} />
+]
+
 const Post = () => {
     return (
-        <></>
+        <>
+            <Header />
+            <div className="margin">
+                <section>
+                    <Heading>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolore.</Heading>
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolore iusto qui asperiores ut dolorem aliquam maxime! Adipisci, quia tempore at neque sint cumque quisquam fuga rerum, veritatis sunt soluta atque!</p>
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolore iusto qui asperiores ut dolorem aliquam maxime! Adipisci, quia tempore at neque sint cumque quisquam fuga rerum, veritatis sunt soluta atque!</p>
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolore iusto qui asperiores ut dolorem aliquam maxime! Adipisci, quia tempore at neque sint cumque quisquam fuga rerum, veritatis sunt soluta atque!</p>
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolore iusto qui asperiores ut dolorem aliquam maxime! Adipisci, quia tempore at neque sint cumque quisquam fuga rerum, veritatis sunt soluta atque!</p>
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolore iusto qui asperiores ut dolorem aliquam maxime! Adipisci, quia tempore at neque sint cumque quisquam fuga rerum, veritatis sunt soluta atque!</p>
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolore iusto qui asperiores ut dolorem aliquam maxime! Adipisci, quia tempore at neque sint cumque quisquam fuga rerum, veritatis sunt soluta atque!</p>
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolore iusto qui asperiores ut dolorem aliquam maxime! Adipisci, quia tempore at neque sint cumque quisquam fuga rerum, veritatis sunt soluta atque!</p>
+                    <hr />
+                    <Heading as="h2" size="small">Recomendações</Heading>
+                    <SliderComponent items={items} />
+                </section>
+            </div>
+        </>
     );
 };
 
