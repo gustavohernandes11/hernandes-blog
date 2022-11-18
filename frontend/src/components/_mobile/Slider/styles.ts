7a78c8253da36eab492379d451cce222c833e958
gustavohernandes11 <gustavo.hernandes.s11@gmail.com>
2022-11-18 20:58:37 -0300

    fix: ajuste no layout do slider em telas pequenas

--- frontend/src/components/_mobile/Slider/styles.ts
+++ frontend/src/components/_mobile/Slider/styles.ts
@@ -35,4 +35,41 @@ export const Container = styled.div`
     .slick-dots li button:before {
         color: var(--gray-200);
     }
+
+    @media (min-width: 600px) {
+        .button-control-next {
+            right: 1rem;
+        }
+        .button-control-prev {
+            left: 1rem;
+        }
+        .slider {
+            .slick-list {
+                margin: 0 -2rem;
+            }
+
+            .slick-slide > div {
+                padding: 0 4rem;
+                margin: 0 auto;
+            }
+        }
+    }
+    @media (max-width: 450px) {
+        .button-control-next {
+            display: none;
+        }
+        .button-control-prev {
+            display: none;
+        }
+        .slider {
+            .slick-list {
+                margin: 0 -2rem;
+            }
+
+            .slick-slide > div {
+                padding: 0 1rem;
+                margin: 0 auto;
+            }
+        }
+    }
 `;
