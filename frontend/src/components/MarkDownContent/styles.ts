2cdf738a15fccb1b76c546d005e6f67c4e421ee2
gustavohernandes11 <gustavo.hernandes.s11@gmail.com>
2022-11-21 16:50:05 -0300

    style: agora listas no markdown tem o mesmo tamanho dos outros textos

--- frontend/src/components/MarkDownContent/styles.ts
+++ frontend/src/components/MarkDownContent/styles.ts
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 export const Container = styled.article`
     .markdown-content {
-        p {
+        p, ul, li {
             font-size: 1.2rem;
         }
         a {
