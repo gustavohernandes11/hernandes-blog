ea56e682ed94aa00ce25ac65fcc1ff0b864bd61b
gustavohernandes11 <gustavo.hernandes.s11@gmail.com>
2022-12-17 13:05:15 -0300

    fix: removed react-responsive

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,22 +1,16 @@
 import type { AppProps } from "next/app";
 import GlobalStyles from "../src/styles/GlobalStyles";
-import { useScreen } from "../src/hooks/useScreen";
 import { MenuContextProvider } from "contexts/MenuContextProvider";
 import { Layout } from "../src/layout";
 
 import { theme } from "../src/theme";
-import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
+import { ChakraProvider } from "@chakra-ui/react";
 
 function MyApp({ Component, pageProps }: AppProps) {
-    const { isTablet } = useScreen();
-
     return (
         <ChakraProvider theme={theme}>
             <GlobalStyles />
             <MenuContextProvider>
-                <ColorModeScript
-                    initialColorMode={theme.config.initialColorMode}
-                />
                 <Layout>
                     <Component {...pageProps} />
                 </Layout>
