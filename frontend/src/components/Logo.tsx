79fa0f536e3aeb49c50da11f776ef27d1148d38d
gustavohernandes11 <gustavo.hernandes.s11@gmail.com>
2023-01-05 19:33:08 -0300

    fix: added link to home page

--- frontend/src/components/Logo.tsx
+++ frontend/src/components/Logo.tsx
@@ -1,6 +1,7 @@
 import LogoIMG from "../../public/Logo.png";
 import LogoExtensiveIMG from "../../public/LogoExtensive.png";
 import Image from "next/image";
+import Link from "next/link";
 
 type LogoProps = {
     variant?: "small" | "extended";
@@ -10,12 +11,12 @@ type LogoProps = {
 
 export const Logo = ({ variant, width, height = 30 }: LogoProps) => {
     return (
-        <>
+        <Link href="/">
             {variant === "small" ? (
                 <Image src={LogoIMG} width={width || 25} alt="logo" />
             ) : (
                 <Image src={LogoExtensiveIMG} height={height} alt="logo" />
             )}
-        </>
+        </Link>
     );
 };
