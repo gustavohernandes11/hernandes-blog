e5c412be35ed065626a56f798137c02a3dec35aa
gustavohernandes11 <gustavo.hernandes.s11@gmail.com>
2023-09-30 20:43:15 -0300

    feat: implement logo scale option

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -3,9 +3,19 @@ import LightLogo from "../../public/LightLogo.svg";
 import DarkLogo from "../../public/DarkLogo.svg";
 import { useThemeContext } from "hooks/useThemeContext";
 
-export const Logo = () => {
+type LogoProps = {
+    scale?: 1 | 1.25 | 1.5 | 1.75 | 2.0;
+};
+export const Logo = ({ scale = 1 }: LogoProps) => {
     const [theme] = useThemeContext();
-    const themedLogo = () => (theme === "light" ? LightLogo : DarkLogo);
+    const getThemedLogo = () => (theme === "light" ? LightLogo : DarkLogo);
 
-    return <Image src={themedLogo()} alt="logotipo" width={130} height={30} />;
+    return (
+        <Image
+            src={getThemedLogo()}
+            alt="logotipo"
+            width={130 * scale}
+            height={30 * scale}
+        />
+    );
 };
