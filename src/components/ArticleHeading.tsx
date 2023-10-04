d6c0a26c572889a875278c7f3dc655f193037652
gustavohernandes11 <gustavo.hernandes.s11@gmail.com>
2023-10-04 20:18:49 -0300

    feat: add custom headings to markdown articles

---
+++ src/components/ArticleHeading.tsx
@@ -0,0 +1,5 @@
+import styled from "styled-components";
+
+export const ArticleHeading = styled.h1`
+    margin-block: 2rem 1rem;
+`;
--- src/utils/mdx-components.tsx
+++ src/utils/mdx-components.tsx
@@ -1,7 +1,38 @@
 import SyntaxHighlighter from "react-syntax-highlighter";
-import { atomDark } from "react-syntax-highlighter/dist/esm/styles/prism";
+import { atomDark } from "react-syntax-highlighter/dist/cjs/styles/prism";
+import { ArticleHeading } from "../components/ArticleHeading";
 
 export const MDXComponents = {
+    h1: ({ children, ...props }: any) => (
+        <ArticleHeading as="h1" {...props}>
+            {children}
+        </ArticleHeading>
+    ),
+    h2: ({ children, ...props }: any) => (
+        <ArticleHeading as="h2" {...props}>
+            {children}
+        </ArticleHeading>
+    ),
+    h3: ({ children, ...props }: any) => (
+        <ArticleHeading as="h3" {...props}>
+            {children}
+        </ArticleHeading>
+    ),
+    h4: ({ children, ...props }: any) => (
+        <ArticleHeading as="h4" {...props}>
+            {children}
+        </ArticleHeading>
+    ),
+    h5: ({ children, ...props }: any) => (
+        <ArticleHeading as="h5" {...props}>
+            {children}
+        </ArticleHeading>
+    ),
+    h6: ({ children, ...props }: any) => (
+        <ArticleHeading as="h6" {...props}>
+            {children}
+        </ArticleHeading>
+    ),
     code({ node, inline, className, style, children, ...props }: any) {
         const match = /language-(\w+)/.exec(className || "");
         return !inline && match ? (
