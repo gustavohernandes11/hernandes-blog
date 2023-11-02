87949c52234ad519ef62b46ca44efe466e7412df
gustavohernandes11 <gustavo.hernandes.s11@gmail.com>
2023-11-02 14:56:35 -0300

    fix: add google font scripts

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -32,6 +32,19 @@ export default class MyDocument extends Document {
         return (
             <Html lang="pt-BR">
                 <Head>
+                    <link
+                        rel="preconnect"
+                        href="https://fonts.googleapis.com"
+                    />
+                    <link
+                        rel="preconnect"
+                        href="https://fonts.gstatic.com"
+                        crossOrigin="true"
+                    />
+                    <link
+                        href="https://fonts.googleapis.com/css2?family=DM+Sans:opsz,wght@9..40,100;9..40,400;9..40,700&family=Ubuntu:wght@300;400;700&display=swap"
+                        rel="stylesheet"
+                    />
                     <meta name="language" content="Portuguese" />
                     <meta name="revisit-after" content="3 days" />
                     <meta name="author" content="Gustavo Hernandes" />
